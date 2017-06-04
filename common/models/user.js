11a017324702770d894cf8d74fa009568baaa444
La-Keisha Towner <ltowner622@gmail.com>
2017-06-03 20:18:30 -0400

    updated spacing

--- common/models/user.js
+++ common/models/user.js
@@ -70,4 +70,26 @@ module.exports = function(User) {
       redirectToLinkText: 'Log in'
     });
   });
+
+  app.post('/login', function(req, res) {
+  User.login({
+    email: req.body.email,
+    password: req.body.password
+  }, 'user', function(err, token) {
+    if (err) {
+      res.render('response', { //render view named 'response.ejs'
+        title: 'Login failed',
+        content: err,
+        redirectTo: '/',
+        redirectToLinkText: 'Try again'
+      });
+      return;
+    }
+
+    res.render('home', { //login user and render 'home' view
+      email: req.body.email,
+      accessToken: token.id
+    });
+  });
+}); 
 };
