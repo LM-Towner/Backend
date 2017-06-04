50c050d50c1b8c4a3f2dc0dffee50f0d02ead073
La-Keisha Towner <ltowner622@gmail.com>
2017-06-04 11:45:24 -0400

    fixed some spacing issues

--- common/models/recommendation.js
+++ common/models/recommendation.js
@@ -1,12 +1,12 @@
 'use strict';
 
 module.exports = function(Recommendation) {
-  Recommendation.count = function(msg, cb){
-    cb(null, 'The total for recommednations are' + msg)
-  }
+  Recommendation.count = function(msg, cb) {
+    cb(null, 'The total for recommednations are' + msg);
+  };
 
   Recommendation.remoteMethod('count', {
-        accepts: {arg: 'msg', type: 'string'},
-        returns: {arg: 'count', type: 'number'}
-      });
+    accepts: {arg: 'msg', type: 'string'},
+    returns: {arg: 'count', type: 'number'},
+  });
 };
--- common/models/user.js
+++ common/models/user.js
@@ -1,6 +1,8 @@
 'use strict';
 var config = require('../../server/config.json');
 var path = require('path');
+var loopback = require('loopback');
+var app = module.exports = loopback();
 
 module.exports = function(User) {
   User.afterRemote('create', function(context, user, next) {
@@ -11,7 +13,7 @@ module.exports = function(User) {
       subject: 'Thanks for registering.',
       template: path.resolve(__dirname, '../../server/views/verify.ejs'),
       redirect: '/verified',
-      user: user
+      user: user,
     };
 
     user.verify(options, function(err, response) {
@@ -22,33 +24,33 @@ module.exports = function(User) {
       context.res.render('response', {
         title: 'Signed up successfully',
         content: 'Please check your email and click on the verification link ' +
-            'before logging in.',
+          'before logging in.',
         redirectTo: '/',
-        redirectToLinkText: 'Log in'
+        redirectToLinkText: 'Log in',
       });
     });
   });
   User.afterRemote('prototype.verify', function(context, user, next) {
     context.res.render('response', {
-      title: 'A Link to reverify your identity has been sent '+
+      title: 'A Link to reverify your identity has been sent ' +
         'to your email successfully',
-      content: 'Please check your email and click on the verification link '+
+      content: 'Please check your email and click on the verification link ' +
         'before logging in',
       redirectTo: '/',
-      redirectToLinkText: 'Log in'
+      redirectToLinkText: 'Log in',
     });
   });
 
   User.on('resetPasswordRequest', function(info) {
     var url = 'http://' + config.host + ':' + config.port + '/reset-password';
     var html = 'Click <a href="' + url + '?access_token=' +
-        info.accessToken.id + '">here</a> to reset your password';
+      info.accessToken.id + '">here</a> to reset your password';
 
     User.app.models.Email.send({
       to: info.email,
       from: info.email,
       subject: 'Password reset',
-      html: html
+      html: html,
     }, function(err) {
       if (err) return console.log('> error sending password reset email');
       console.log('> sending password reset email to:', info.email);
@@ -59,7 +61,7 @@ module.exports = function(User) {
       title: 'Password changed successfully',
       content: 'Please login again with new password',
       redirectTo: '/',
-      redirectToLinkText: 'Log in'
+      redirectToLinkText: 'Log in',
     });
   });
   User.afterRemote('setPassword', function(context, user, next) {
@@ -67,29 +69,29 @@ module.exports = function(User) {
       title: 'Password reset success',
       content: 'Your password has been reset successfully',
       redirectTo: '/',
-      redirectToLinkText: 'Log in'
+      redirectToLinkText: 'Log in',
     });
   });
 
   app.post('/login', function(req, res) {
-  User.login({
-    email: req.body.email,
-    password: req.body.password
-  }, 'user', function(err, token) {
-    if (err) {
-      res.render('response', {
-        title: 'Login failed',
-        content: err,
-        redirectTo: '/',
-        redirectToLinkText: 'Try again'
-      });
-      return;
-    }
-
-    res.render('home', {
+    User.login({
       email: req.body.email,
-      accessToken: token.id
+      password: req.body.password,
+    }, 'user', function(err, token) {
+      if (err) {
+        res.render('response', {
+          title: 'Login failed',
+          content: err,
+          redirectTo: '/',
+          redirectToLinkText: 'Try again',
+        });
+        return;
+      }
+
+      res.render('home', {
+        email: req.body.email,
+        accessToken: token.id,
+      });
     });
-  });
   });
 };
